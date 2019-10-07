257df150e8cc9f2d2da63692bcff262b82d4b30d
Chris Alegre <calegre14@gmail.com>
2019-10-07 14:52:24 -0600

    Initial Commit

---
+++ JournalObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JournalObjC
+//
+//  Created by Christopher Alegre on 10/7/19.
+//  Copyright © 2019 Christopher Alegre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JournalObjC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  JournalObjC
+//
+//  Created by Christopher Alegre on 10/7/19.
+//  Copyright © 2019 Christopher Alegre. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
